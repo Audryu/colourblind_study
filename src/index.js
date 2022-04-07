58f96376090a8c3b58335e1259f23a9499f9ae98
Stephen Thompson <s.thompson@ucl.ac.uk>
2022-04-07 15:29:37 +0100

    Issue #1 removed video, added static image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,73 @@
-index_opengl.js
+import '@kitware/vtk.js/favicon.js';
+
+// Load the rendering pieces we want to use (for both WebGL and WebGPU)
+import '@kitware/vtk.js/Rendering/Profiles/Geometry.js';
+
+import vtkActor from '@kitware/vtk.js/Rendering/Core/Actor.js';
+import vtkConeSource from '@kitware/vtk.js/Filters/Sources/ConeSource.js';
+import vtkMapper from '@kitware/vtk.js/Rendering/Core/Mapper.js';
+import vtkOpenGLRenderWindow from '@kitware/vtk.js/Rendering/OpenGL/RenderWindow.js';
+import vtkRenderWindow from '@kitware/vtk.js/Rendering/Core/RenderWindow.js';
+import vtkRenderWindowInteractor from '@kitware/vtk.js/Rendering/Core/RenderWindowInteractor.js';
+import vtkRenderer from '@kitware/vtk.js/Rendering/Core/Renderer.js';
+import vtkInteractorStyleTrackballCamera from '@kitware/vtk.js/Interaction/Style/InteractorStyleTrackballCamera.js';
+
+import {coneactor} from './sksAnatomy.js'
+// ----------------------------------------------------------------------------
+// Standard rendering code setup
+// ----------------------------------------------------------------------------
+
+const renderWindow = vtkRenderWindow.newInstance();
+const renderer = vtkRenderer.newInstance({ background: [0.0, 0.0, 0.0, 0.0] });
+renderWindow.addRenderer(renderer);
+
+// ----------------------------------------------------------------------------
+// Simple pipeline ConeSource --> Mapper --> Actor
+// ----------------------------------------------------------------------------
+
+const actor = coneactor();
+
+// ----------------------------------------------------------------------------
+// Add the actor to the renderer and set the camera based on it
+// ----------------------------------------------------------------------------
+
+renderer.addActor(actor);
+renderer.resetCamera();
+
+// ----------------------------------------------------------------------------
+// Use OpenGL as the backend to view the all this
+// ----------------------------------------------------------------------------
+
+const openglRenderWindow = vtkOpenGLRenderWindow.newInstance();
+
+renderWindow.addView(openglRenderWindow);
+
+// ----------------------------------------------------------------------------
+// Create a div section to put this into
+// ----------------------------------------------------------------------------
+
+const container = document.getElementById('foreground');
+openglRenderWindow.setContainer(container);
+
+// ----------------------------------------------------------------------------
+// Capture size of the video window and set it to the renderWindow
+// ----------------------------------------------------------------------------
+const background = document.getElementById('background');
+const { width, height } = background.getBoundingClientRect();
+openglRenderWindow.setSize(width, height);
+
+// ----------------------------------------------------------------------------
+// Setup an interactor to handle mouse events
+// ----------------------------------------------------------------------------
+
+const interactor = vtkRenderWindowInteractor.newInstance();
+interactor.setView(openglRenderWindow);
+interactor.initialize();
+interactor.bindEvents(container);
+
+// ----------------------------------------------------------------------------
+// Setup interactor style to use
+// ----------------------------------------------------------------------------
+
+interactor.setInteractorStyle(vtkInteractorStyleTrackballCamera.newInstance());
+
