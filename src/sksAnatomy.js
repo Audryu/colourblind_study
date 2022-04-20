3a3346aa2677ae46ec2eb9f77e1102a2f6bb4774
Stephen Thompson <s.thompson@ucl.ac.uk>
2022-04-20 14:45:06 +0100

    Issue #1 removed test directory from lint, removed semicolons

--- src/sksAnatomy.js
+++ src/sksAnatomy.js
@@ -1,35 +1,35 @@
 // Pull the logic for making anatomy actors out of main loop
-import vtkConeSource from '@kitware/vtk.js/Filters/Sources/ConeSource.js';
-import vtkMapper from '@kitware/vtk.js/Rendering/Core/Mapper.js';
-import vtkActor from '@kitware/vtk.js/Rendering/Core/Actor.js';
-import vtkXMLPolyDataReader from '@kitware/vtk.js/IO/XML/XMLPolyDataReader.js';
+import vtkConeSource from '@kitware/vtk.js/Filters/Sources/ConeSource.js'
+import vtkMapper from '@kitware/vtk.js/Rendering/Core/Mapper.js'
+import vtkActor from '@kitware/vtk.js/Rendering/Core/Actor.js'
+import vtkXMLPolyDataReader from '@kitware/vtk.js/IO/XML/XMLPolyDataReader.js'
 
 export function coneactor() {
 
-	const coneSource = vtkConeSource.newInstance({ height: 1.0 });
+	const coneSource = vtkConeSource.newInstance({ height: 1.0 })
 
-	const mapper = vtkMapper.newInstance();
-	mapper.setInputConnection(coneSource.getOutputPort());
+	const mapper = vtkMapper.newInstance()
+	mapper.setInputConnection(coneSource.getOutputPort())
 
-	const sksactor = vtkActor.newInstance();
-	sksactor.setMapper(mapper);
-	sksactor.getProperty().setOpacity(0.5);
+	const sksactor = vtkActor.newInstance()
+	sksactor.setMapper(mapper)
+	sksactor.getProperty().setOpacity(0.5)
 
-	return sksactor;
+	return sksactor
 }
 
 export function tumouractor(callback) {
-	const reader = vtkXMLPolyDataReader.newInstance();
+	const reader = vtkXMLPolyDataReader.newInstance()
 	const filename='011_tumor.vtp'
 	reader.setUrl(`assets/${filename}`).then(() => {
-  		const polydata = reader.getOutputData(0);
-  		const mapper = vtkMapper.newInstance();
-  		const actor = vtkActor.newInstance();
+  		const polydata = reader.getOutputData(0)
+  		const mapper = vtkMapper.newInstance()
+  		const actor = vtkActor.newInstance()
 
-  		actor.setMapper(mapper);
-  		mapper.setInputData(polydata);
-		callback(false, actor);
-	});
+  		actor.setMapper(mapper)
+  		mapper.setInputData(polydata)
+		callback(false, actor)
+	})
 }
 
 
