78e1a68e1d7c422dda146bc1f951a943422d434b
Stephen Thompson <s.thompson@ucl.ac.uk>
2022-05-04 14:00:21 +0100

    Issue #3 added a test for lights

--- src/lights.js
+++ src/lights.js
@@ -2,7 +2,7 @@ import vtkLight from '@kitware/vtk.js/Rendering/Core/Light.js'
 
 export function setupLights (url, renderer) {
   const spotLightOn = url.searchParams.get('spotlight')
-  if (spotLightOn != null) {
+  if (spotLightOn !== null) {
     const light0 = vtkLight.newInstance()
     light0.setLightTypeToSceneLight()
     light0.setFocalPoint(150, 0, 0)
