d2ef3072bfddbc7d8d27ac4f9368f866f8bb9587
Audryu <123420838+Audryu@users.noreply.github.com>
2023-03-10 18:04:45 +0000

    Update anatomy.js
    Added portal artery, portal vein and hepatic vein. Changed the opacity of the liver to try simulate AR with mesh

--- src/anatomy.js
+++ src/anatomy.js
@@ -10,9 +10,9 @@ export function liveractor (urlOptions, callback) {
   const id = 'liver'
   const defaultColour = '6600FF'
   const actor = vtkActor.newInstance()
-
+  const opacity = 0.3
   const {
-    diffuseColour, specularColour, diffusePower, specularPower, opacity
+    diffuseColour, specularColour, diffusePower, specularPower
   } = unpackOptions(id, urlOptions, defaultColour)
   applyProperties(actor, diffuseColour, specularColour,
     diffusePower, specularPower, opacity)
@@ -65,18 +65,51 @@ export function gallbladderactor (urlOptions, callback) {
       callback(err, null)
     })
 }
-export function tumoractor (urlOptions, callback) {
-  const id = 'tumor'
-  const defaultColour = '22FF22'
 
+export function hepaticveinactor (urlOptions, callback) {
+  const id = 'v1'
+  const defaultColour = 'FF0000'
   const actor = vtkActor.newInstance()
+  const mapper = vtkMapper.newInstance() // Define the mapper variable
+
   const {
     diffuseColour, specularColour, diffusePower, specularPower, opacity
   } = unpackOptions(id, urlOptions, defaultColour)
   applyProperties(actor, diffuseColour, specularColour,
     diffusePower, specularPower, opacity)
+
   const reader = vtkXMLPolyDataReader.newInstance()
-  const filename = 'assets/tumor.vtp'
+  const filename = 'assets/hepaticveins.vtp'
+  let error = false
+  reader.setUrl(path + filename).then(() => {
+    const polydata = reader.getOutputData(0)
+
+    actor.setMapper(mapper)
+    mapper.setInputData(polydata)
+    actor.setPosition(-10, 20, 160)
+    actor.rotateWXYZ(230, 1, 0, 0)
+    actor.rotateWXYZ(190, 0, 0, 1)
+    callback(error, actor)
+  })
+    .catch(err => {
+      error = true
+      callback(err, null)
+    })
+  }
+
+export function portalveinactor (urlOptions, callback) {
+  const id = 'v2'
+  const defaultColour = '00FF00'
+  const actor = vtkActor.newInstance()
+
+  const {
+    diffuseColour, specularColour, diffusePower, specularPower, opacity
+  } = unpackOptions(id, urlOptions, defaultColour)
+  applyProperties(actor, diffuseColour, specularColour,
+    diffusePower, specularPower, opacity)
+
+  const reader = vtkXMLPolyDataReader.newInstance()
+  const filename = 'assets/portalvein.vtp'
   let error = false
   reader.setUrl(path + filename).then(() => {
     const polydata = reader.getOutputData(0)
@@ -84,7 +117,10 @@ export function tumoractor (urlOptions, callback) {
 
     actor.setMapper(mapper)
     mapper.setInputData(polydata)
-    actor.setPosition(30, 0, 10)
+    actor.setPosition(10, 6, 80)
+    actor.rotateWXYZ(-270, 1, 0, 0)
+    actor.rotateWXYZ(350, 0, 0, 1)
+    actor.rotateWXYZ(190, 0, 1, 0)
     callback(error, actor)
   })
     .catch(err => {
@@ -93,6 +129,38 @@ export function tumoractor (urlOptions, callback) {
     })
 }
 
+export function arteryactor (urlOptions, callback) {
+  const id = 'artery'
+  const defaultColour = '00FF00'
+  const actor = vtkActor.newInstance()
+
+  const {
+    diffuseColour, specularColour, diffusePower, specularPower, opacity
+  } = unpackOptions(id, urlOptions, defaultColour)
+  applyProperties(actor, diffuseColour, specularColour,
+    diffusePower, specularPower, opacity)
+
+  const reader = vtkXMLPolyDataReader.newInstance()
+  const filename = 'assets/arteries.vtp'
+  let error = false
+  reader.setUrl(path + filename).then(() => {
+    const polydata = reader.getOutputData(0)
+    const mapper = vtkMapper.newInstance()
+
+    actor.setMapper(mapper)
+    mapper.setInputData(polydata)
+    actor.setPosition(-5, 65, 90)
+    actor.rotateWXYZ(230, 1, 0, 0)
+    actor.rotateWXYZ(100, 0, 1, 0)
+    actor.rotateWXYZ(-20, 0, 0 ,1)
+
+    callback(error, actor)
+  })
+    .catch(err => {
+      error = true
+      callback(err, null)
+    })
+}
 function hexToRgb (hex) {
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
   return result
